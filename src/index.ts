5f473f17c353d8e5f0741d2c75b70e07f1bb69d8
Szymon Wygnanski <s@finalclass.net>
2016-10-31 08:21:10 +0100

    adding async/await for .on method

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,25 @@ import * as cote from 'cote';
 import SingleInit from 'single-init';
 
 export interface ICoteEvent {
-    type:string;
+    type: string;
+}
+
+export interface ICoteRequest {
+    val: any;
 }
 
 export class Requester {
 
-    private client:SingleInit<any>;
+    private client: SingleInit<any>;
 
-    constructor(name:string) {
+    constructor(name: string) {
         this.client = new SingleInit((done) => {
-            let client = new cote.Requester({name});
+            let client = new cote.Requester({ name });
             client.on('ready', () => done(client));
         });
     }
 
-    public send(event:ICoteEvent) {
+    public send(event: ICoteEvent) {
         return this.client.get().then((client) => {
             return new Promise((resolve) => {
                 client.get(event, resolve);
@@ -28,4 +32,17 @@ export class Requester {
 
 }
 
-export var Responder = cote.Responder;
+export class Responder {
+
+    private coteResponder: any;
+
+    constructor(advertisement: { name: string; respondsTo: string[] }) {
+        this.coteResponder = new cote.Responder(advertisement);
+    }
+
+    on(eventType: string, handler: (req: ICoteRequest) => Promise<any>) {
+        this.coteResponder.on(eventType, (req:ICoteRequest, done) => {
+            handler(req).then(done).catch((err) => {throw err});
+        });
+    }
+}
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,12 +1,7 @@
 declare module "cote" {
-    export var Requester:any;
-    export class Responder {
-        constructor(advertisement:{name:string;respondsTo:string[]});
-        on(eventType:string, handler:(req:ICoteRequest, callback:(response?:any)=>void)=>void):void;
-    }
-    export var Publisher:any;
-    export var Subscriber:any;
-    export interface ICoteRequest {
-        val:any;
-    }
+    export var Requester: any;
+    export var Responder: any;
+    export var Publisher: any;
+    export var Subscriber: any;
+
 }
